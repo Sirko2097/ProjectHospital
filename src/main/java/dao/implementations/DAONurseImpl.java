c1fa36c0444198690288435c67470a80d2150a45
Serhii <sirko2097@gmail.com>
2018-05-20 14:17:43 +0300

    DAONurse interface

--- src/main/java/dao/implementations/DAONurseImpl.java
+++ src/main/java/dao/implementations/DAONurseImpl.java
@@ -1,4 +1,13 @@
 package dao.implementations;
 
+import java.sql.Connection;
+
 public class DAONurseImpl {
+    private final Connection connection;
+
+    public DAONurseImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+
 }
--- src/main/java/dao/interfaces/DAONurse.java
+++ src/main/java/dao/interfaces/DAONurse.java
@@ -1,4 +1,11 @@
 package dao.interfaces;
 
+import model.Nurse;
+
+import java.sql.SQLException;
+
 public interface DAONurse {
+    Nurse read(String key) throws SQLException;
+
+    boolean nurseLogi(String licenseNumber, String password);
 }
