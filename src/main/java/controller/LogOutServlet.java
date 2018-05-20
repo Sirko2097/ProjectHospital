356ff08862f6fb99cdf7082995d043ba99a423d7
Serhii <sirko2097@gmail.com>
2018-05-20 20:20:08 +0300

    Logout Servlet

---
+++ src/main/java/controller/LogOutServlet.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogOutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        service(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        service(req, resp);
+    }
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) {
+        request.getSession().invalidate();
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -9,9 +9,12 @@
 <html>
   <head>
     <title>Index</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body class="container">
     <h1>Welcome!</h1>
-    <input type="button" class="btn btn-info" onclick="location.href='/login'" formmethod="post" value="Log In"/>
+    <button type="button" class="btn btn-primary" onclick="location.href='/login'">Log In</button>
   </body>
 </html>
