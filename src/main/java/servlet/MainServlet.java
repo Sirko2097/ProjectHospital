93a594e30f2f3cb368bc83a4cee16e2bb7c29b37
Serhii <sirko2097@gmail.com>
2018-05-22 00:06:08 +0300

    Basic ideas of solving problems with getting doctor's(nurse) patients.

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -1,11 +1,20 @@
 package servlet;
 
+import dao.implementations.DAOFactoryImpl;
+import model.Patient;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -16,13 +25,31 @@ import java.io.IOException;
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        try {
+            getAllDoctorPatients(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        try {
+            getAllDoctorPatients(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    private void getAllDoctorPatients(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+        Connection connection = DAOFactoryImpl.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("");
+        ResultSet resultSet = preparedStatement.executeQuery();
 
+        List<Patient> patients = new ArrayList<>();
+
+        while (resultSet.next()) {
+
+        }
+    }
 }
